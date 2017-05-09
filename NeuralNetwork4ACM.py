0763c84bdfe17ae14f5b45b3ab99d69051b5533b
Jay <schenxmu@gmail.com>
2017-05-09 11:28:02 +0800

    exists bugs

--- NeuralNetwork4ACM.py
+++ NeuralNetwork4ACM.py
@@ -57,9 +57,9 @@ def load_data(genre, batch_size=100):
     # filter broken data in datalist
     for filename, y in zip(filenames, labels):
         try:
-            print('../data/face_landmark_all/'+filename.replace('jpg','npy'))
+            # print('../data/face_landmark_all/'+filename.replace('jpg','npy'))
             x = np.load('../data/face_landmark_all/'+filename.replace('jpg','npy'))
-            xs.append(x)
+            xs.append(np.reshape(x,x.shape[0]*x.shape[1]))
             ys.append(y)
         except:
             pass
@@ -72,12 +72,27 @@ def load_data(genre, batch_size=100):
     num_examples = len(xs)
     assert(num_examples > 0)
     print('num_examples:',num_examples)
-    min_after_dequeue = 1000
-    capacity = min_after_dequeue + 3 * batch_size
-    example_batch, label_batch = tf.train.shuffle_batch(
-        [xs, ys], batch_size=batch_size, capacity=capacity,
-        min_after_dequeue=min_after_dequeue)
-    return example_batch, label_batch, num_examples
+    xs_mat = np.zeros((num_examples, len(xs[0])))
+    ys_mat = np.zeros((num_examples, 1))
+    for line in range(num_examples):
+        xs_mat[line] = xs[line]	
+        ys_mat[line] = ys[line]
+    return xs_mat, ys_mat, num_examples
+
+def data_iterator(xs, ys, batch_size):
+    """ A simple data iterator """
+    batch_idx = 0
+    while True:
+        # shuffle labels and features
+        idxs = np.arange(0, len(xs))
+        np.random.shuffle(idxs)
+        shuf_features = xs[idxs]
+        shuf_labels = ys[idxs]
+        for batch_idx in range(0, len(xs), batch_size):
+            xs_batch = shuf_features[batch_idx:batch_idx+batch_size] / float(batch_size-1)
+            xs_batch = xs_batch.astype("float32")
+            ys_batch = shuf_labels[batch_idx:batch_idx+batch_size]
+            yield xs_batch, ys_batch
 
 # 1.训练的数据
 lr = 0.001
@@ -88,7 +103,9 @@ batch_size = 100
 display_step = 100
 
 # Make up some real data 
-xs_batch, ys_batch, num_examples = load_data(genre = 'train', batch_size = batch_size)
+xs_mat, ys_mat, num_examples = load_data(genre = 'train', batch_size = batch_size)
+# 定义迭代器
+iter_ = data_iterator(xs_mat, ys_mat, batch_size)
 
 # 2.定义节点准备接收数据
 # define placeholder for inputs to network  
@@ -96,12 +113,11 @@ xs = tf.placeholder(tf.float32, [None, 136])
 ys = tf.placeholder(tf.float32, [None, 1])
 
 # 3.定义神经层：隐藏层和预测层
-# add hidden layer 输入值是 xs，在隐藏层有 10 个神经元   
+# add hidden layer 输入值是 xs，在隐藏层有 800 个神经元   
 l1 = add_layer(xs, 136, 800, 'l1', activation_function=tf.nn.relu, dropout=dropout)
 l2 = add_layer(l1, 800, 800, 'l2', activation_function=tf.nn.relu, dropout=dropout)
 l3 = add_layer(l2, 800, 300, 'l3', activation_function=tf.nn.relu, dropout=dropout)
-
-# add output layer 输入值是隐藏层 l1，在预测层输出 1 个结果
+# add output layer 输入值是隐藏层 l3，在预测层输出 3 个结果
 prediction = add_layer(l3, 300, 2, 'output', activation_function=None)
 
 # 4.定义 loss 表达式
@@ -111,7 +127,7 @@ loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
 
 # 5.选择 optimizer 使 loss 达到最小                   
 # 这一行定义了用什么方式去减少loss，学习率是 0.001    
-train_step = tf.train.GradientDescentOptimizer(lr).minimize(loss)
+optimizer = tf.train.GradientDescentOptimizer(lr).minimize(loss)
 
 # important step 对所有变量进行初始化
 init = tf.initialize_all_variables()
@@ -120,6 +136,7 @@ init = tf.initialize_all_variables()
 with tf.Session() as sess:
     sess.run(init)
     # Training cycle
+    '''
     print
     print(ys_batch)
     print
@@ -129,9 +146,11 @@ with tf.Session() as sess:
         total_batch = int(num_examples/batch_size)
         # Loop over all batches
         for i in range(total_batch):
+            # get a batch of data
+            xs_batch, ys_batch = iter_.next()
             # Run optimization op (backprop) and cost op (to get loss value)
-            _, c = sess.run([optimizer, cost], feed_dict={x: xs_batch,
-                                                          y: ys_batch})
+            _, c = sess.run([optimizer, loss], feed_dict={xs: xs_batch,
+                                                        ys: ys_batch})
             # Compute average loss
             avg_cost += c / total_batch
         # Display logs per epoch step
@@ -139,17 +158,17 @@ with tf.Session() as sess:
             print("Epoch:", '%04d' % (epoch+1), "cost=", \
                 "{:.9f}".format(avg_cost))
     print("Optimization Finished!")
-    '''
+    # '''
 
     # 用 saver 将所有的 variable 保存到定义的路径
     saver = tf.train.Saver()
 
     # 用 saver 将所有的 variable 保存到定义的路径
-    save_path = saver.save(sess, "../models/save_net.ckpt")
+    save_path = saver.save(sess, "../models/deep_beauty_predictor_net.ckpt")
     print("Save to path: ", save_path)
 
     print('Computing accuracy in val set')
-    v_xs_batch, v_ys_batch ,_ = load_data(genre = 'val', batch_size = batch_size)
+    v_xs_batch, v_ys_batch, _ = load_data(genre = 'val', batch_size = batch_size)
     print(compute_accuracy(v_xs_batch, v_ys_batch))
 
 
